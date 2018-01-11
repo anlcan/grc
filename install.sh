9c5809612ffa08b9a72ad07f506ae70c6015d2c8
anilcanbaykal@gmail.com <anilcanbaykal@gmail.com>
2018-01-11 15:41:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt install grc
 
-sudo wget ''  /usr/share/grc/conf.log4j
+sudo wget 'https://raw.githubusercontent.com/anlcan/grc/master/conf.log4j' -o /usr/share/grc/conf.log4j
 
 cat <<EOF | sudo tee -a /etc/grc.conf
 # docker service logs command
